32b3ee4471ec7fbe41be0e76ac8d78baa93194f8
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2024-12-15 21:21:56 -0300

    novo endpoint

---
+++ src/main/java/com/fagnerdev/curso/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.fagnerdev.curso.controller;
+
+
+import com.fagnerdev.curso.entities.User;
+import com.fagnerdev.curso.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = userService.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
+    }
+}
