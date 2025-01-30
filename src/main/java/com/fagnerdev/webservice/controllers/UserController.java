6c6afce6b1cfc90b190e49bba3a8867bc23ddde8
fagnerdev2024 <fagnertavaresdemenezes2024@gmail.com>
2025-01-30 14:26:42 -0300

    perfil dev

--- src/main/java/com/fagnerdev/webservice/controllers/UserController.java
+++ src/main/java/com/fagnerdev/webservice/controllers/UserController.java
@@ -15,37 +15,37 @@ import java.util.List;
 public class UserController {
 
     @Autowired
-    private UserService service;
+    private UserService userService;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll() {
-        List<User> list = service.findAll();
+        List<User> list = userService.findAll();
         return ResponseEntity.ok().body(list);
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id) {
-        User obj = service.findById(id);
-        return ResponseEntity.ok().body(obj);
+        User user = userService.findById(id);
+        return ResponseEntity.ok().body(user);
     }
 
     @PostMapping
-    public ResponseEntity<User> insert(@RequestBody User obj) {
-        obj = service.insert(obj);
+    public ResponseEntity<User> insert(@RequestBody User user) {
+        user = userService.insert(user);
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
-                .buildAndExpand(obj.getId()).toUri();
-        return ResponseEntity.created(uri).body(obj);
+                .buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
-        service.delete(id);
+        userService.delete(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping(value = "/{id}")
-    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
-        obj = service.update(id, obj);
-        return ResponseEntity.ok().body(obj);
+    public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User user) {
+        user = userService.update(id, user);
+        return ResponseEntity.ok().body(user);
     }
 }
